1d020dbf58b182ac17e0864e3e743284b334db1e
Silvije Kalabic <28826780+kalabic@users.noreply.github.com>
2025-10-22 21:22:46 +0000

    Update ConsoleCancellationSource.

--- src/DotBaseLib/Tools/ConsoleCancellationSource.cs
+++ src/DotBaseLib/Tools/ConsoleCancellationSource.cs
@@ -1,35 +1,150 @@
-﻿using DotBase.Log;
+﻿using DotBase.Core;
+using DotBase.Event;
+using DotBase.Log;
 
 namespace DotBase.Tools;
 
 
-public class ConsoleCancellationSource
+/// <summary>
+/// 
+/// Triggers cancellation only once until object state is reset again using <see cref="Reset(bool)"/>.
+/// 
+/// </summary>
+public class ConsoleCancellationSource 
+    : DisposableBase
 {
+    // Public properties >>
+
+    public bool ContinueExec { get { return _continueExec; } set { _continueExec = value; } }
+
+    public bool IsCancellationRequested { get { return _cts.IsCancellationRequested; } }
+
     public CancellationToken Token { get { return _cts.Token; } }
 
-    public CancellationTokenSource TokenSource { get { return _cts; } }
+    public IEventProducer<CancellationEvent> Cancelled { get { return _cancelledEvent; } }
+
+    // Private data >>
 
 
-    private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+    /// <summary>
+    /// 
+    /// For now this lock only applies to:
+    /// <list type="bullet">
+    /// <item> <see cref="Dispose(bool)"/> </item>
+    /// <item> <see cref="Reset(bool)"/> </item>
+    /// <item> <see cref="HandleCancelKeyPress(object?, ConsoleCancelEventArgs)"/> </item>
+    /// </list>
+    /// 
+    /// </summary>
+    private readonly object _disposeLock = new object();
 
-    private readonly ConsoleCancelEventHandler _sessionCanceler;
+    private bool _continueExec;
 
+    private CancellationEventProducer _cancelledEvent;
+
+    private CancellationTokenSource _cts;
+
+    private volatile int _isCancelled = 0;
+
+    // Implementation >>
 
     public ConsoleCancellationSource()
     {
-        _sessionCanceler = (sender, e) =>
+        _cancelledEvent = new CancellationEventProducer();
+        _cts = new CancellationTokenSource();
+        Console.CancelKeyPress += HandleCancelKeyPress;
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
         {
-            LiteLog.Log.SystemEvent("Console event (Ctrl-C)");
+            lock(_disposeLock)
+            {
+                Unregister();
+                _cts.Dispose();
+                base.Dispose(disposing);
+                return;
+            }
+        }
+        base.Dispose(disposing);
+    }
 
-            _cts.CancelAsync();
-            e.Cancel = true; // Execution continues after the delegate.
-        };
-
-        Console.CancelKeyPress += _sessionCanceler;
+    public void Reset(bool continueExec = false)
+    {
+        lock(_disposeLock)
+        {
+            if (!IsDisposed)
+            {
+                _isCancelled = 0;
+                _continueExec = continueExec;
+                _cts.Dispose();
+                _cts = new CancellationTokenSource();
+            }
+        }
     }
 
     public void Unregister()
     {
-        Console.CancelKeyPress -= _sessionCanceler;
+        Console.CancelKeyPress -= HandleCancelKeyPress;
+    }
+
+    private void HandleCancelKeyPress(object? s, ConsoleCancelEventArgs ev)
+    {
+        LiteLog.Log.SystemEvent("Console event (Ctrl-C)");
+        lock (_disposeLock)
+        {
+            if (TryBeginCancel())
+            {
+                _cts.CancelAsync();
+                _cancelledEvent.Invoke();
+            }
+            ev.Cancel = _continueExec;
+        }
+    }
+
+    public void Cancel() 
+    {
+        if (TryBeginCancel())
+        {
+            _cts.Cancel();
+            _cancelledEvent.Invoke();
+        }
+    }
+
+    public void Cancel(bool throwOnFirstException) 
+    {
+        if (TryBeginCancel())
+        {
+            _cts.Cancel(throwOnFirstException);
+            _cancelledEvent.Invoke();
+        }
+    }
+
+    public Task CancelAsync()
+    {
+        if (TryBeginCancel())
+        {
+            var task = _cts.CancelAsync();
+            _cancelledEvent.Invoke();
+            return task;
+        }
+        else
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+
+    /// <summary>
+    /// <list type="bullet">
+    /// <item> Returns true only once until object state is reset again using <see cref="Reset(bool)"/>. </item>
+    /// <item> Once disposed, it returns false always. </item>
+    /// </list>
+    /// </summary>
+    /// <returns></returns>
+    private bool TryBeginCancel()
+    {
+        return !IsDisposed && Interlocked.CompareExchange(ref _isCancelled, 1, 0) == 0;
     }
 }
