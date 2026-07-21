a074479fa6fc2ec9114bf9796afb8975622ccb85
Silvije Kalabic <28826780+kalabic@users.noreply.github.com>
2026-07-21 22:44:09 +0000

    Add CircularBufferLocked and CircularBufferWaitable.

---
+++ src/DotBaseLib/Buffers/CircularBufferLocked.cs
@@ -0,0 +1,52 @@
+﻿namespace DotBase.Buffers;
+
+
+public class CircularBufferLocked : CircularBufferUnlocked
+{
+    private readonly object _lock = new object();
+
+    public CircularBufferLocked(int size)
+        : base(size)
+    {
+    }
+
+    //
+    // Group: Write
+    //
+
+    public override int Write(byte[] data, int offset, int length)
+    {
+        lock (_lock)
+        {
+            return base.Write(data, offset, length);
+        }
+    }
+
+    //
+    // Group: Read, Advance, ClearBuffer
+    //
+
+    public override int Read(byte[] data, int offset, int length)
+    {
+        lock (_lock)
+        {
+            return base.Read(data, offset, length);
+        }
+    }
+
+    public override void Advance(int count)
+    {
+        lock (_lock)
+        {
+            base.Advance(count);
+        }
+    }
+
+    public override void ClearBuffer()
+    {
+        lock (_lock)
+        {
+            base.ClearBuffer();
+        }
+    }
+}
